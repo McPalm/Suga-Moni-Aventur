fbfb60006e5152c3bd27d9882de26646bd1c403c
palm <tauman.exe@gmail.com>
2019-07-19 21:55:22 +0200

    Finalized the Hellcome level noone is gonna beat that you know. hot garbagage.

--- Assets/Hacks/PerlinNoiseScatterBecauseFuckYou.cs
+++ Assets/Hacks/PerlinNoiseScatterBecauseFuckYou.cs
@@ -7,19 +7,21 @@ public class PerlinNoiseScatterBecauseFuckYou : MonoBehaviour
     public float spread = 1f;
     public float speed = 1f;
 
+    float seed;
 
     Vector3 home;
 
     private void Start()
     {
         home = transform.localPosition;
+        seed = Random.value * 11111f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.localPosition = home + new Vector3(
-            .5f - Mathf.PerlinNoise(Time.timeSinceLevelLoad * speed, .1f),
-            .5f - Mathf.PerlinNoise(Time.timeSinceLevelLoad * speed, 7.1f)) * spread;
+            .5f - Mathf.PerlinNoise(Time.timeSinceLevelLoad * speed, seed),
+            .5f - Mathf.PerlinNoise(Time.timeSinceLevelLoad * speed, seed + 8.51f)) * spread;
     }
 }
